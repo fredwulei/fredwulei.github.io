0505b260f09ae3ce4f118b7be468994119ce8b0f
Fred Wu <fred.wulei@gmail.com>
2015-12-07 12:04:50 -0600

    init repo

---
+++ static/js/index.js
@@ -0,0 +1,41 @@
+// (c) 2015 Lei Wu
+
+
+
+$(function() {
+
+
+  $(window).scroll(function (event) {
+    var scroll = $(window).scrollTop();
+    if(scroll > 43){
+      // alert('123');
+      $('header').addClass('fixed-header');
+      $('.height-holder').show();
+    }else{
+      $('header').removeClass('fixed-header');
+      $('.height-holder').hide();
+    }
+  });
+
+
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+    }
+  });
+
+
+    // alert(123);
+    // $('.sec1').velocity({
+    //     properties: { opacity: 1 },
+    //     options: { duration: 2000 }
+    // });
+});
+
